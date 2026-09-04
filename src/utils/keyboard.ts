1baf876c891292fce2a65482c98ebaf0129d186e
NOX PRIMEEE <noxprimeee80@gmail.com>
2026-09-04 15:28:30 +0000

    Update keyboard.ts

--- src/utils/keyboard.ts
+++ src/utils/keyboard.ts
@@ -1,27 +1,46 @@
-// src/utils/keyboard.ts - Version ultra-simplifiée
 import { Markup } from 'telegraf';
 
 export const mainKeyboard = Markup.inlineKeyboard([
-  [Markup.button.callback('💰 Balance', 'balance')],
-  [Markup.button.callback('🆕 Create Server', 'create')],
-  [Markup.button.callback('🔗 Referral', 'referral')],
-  [Markup.button.callback('❓ Help', 'help')]
+  [
+    Markup.button.callback('💰 Solde', 'balance'),
+    Markup.button.callback('📊 Serveurs', 'servers')
+  ],
+  [
+    Markup.button.callback('🆕 Créer', 'create'),
+    Markup.button.callback('🔗 Parrainage', 'referral')
+  ],
+  [
+    Markup.button.callback('❓ Aide', 'help'),
+    Markup.button.callback('👑 Admin', 'admin')
+  ]
 ]);
 
 export const createServerKeyboard = Markup.inlineKeyboard([
-  [Markup.button.callback('Minecraft', 'create_minecraft')],
-  [Markup.button.callback('Web', 'create_web')],
-  [Markup.button.callback('Game', 'create_game')],
-  [Markup.button.callback('🔙 Back', 'back_main')]
+  [
+    Markup.button.callback('🖥️ Minecraft', 'create_minecraft'),
+    Markup.button.callback('🌐 Web', 'create_web')
+  ],
+  [
+    Markup.button.callback('🎮 Game', 'create_game'),
+    Markup.button.callback('📦 Autre', 'create_other')
+  ],
+  [Markup.button.callback('🔙 Retour', 'back_main')]
 ]);
 
 export const adminKeyboard = Markup.inlineKeyboard([
-  [Markup.button.callback('Users', 'admin_users')],
-  [Markup.button.callback('Coins', 'admin_coins')],
-  [Markup.button.callback('🔙 Back', 'back_main')]
+  [
+    Markup.button.callback('👥 Utilisateurs', 'admin_users'),
+    Markup.button.callback('💰 Coins', 'admin_coins')
+  ],
+  [
+    Markup.button.callback('📊 Stats', 'admin_stats'),
+    Markup.button.callback('⚙️ Paramètres', 'admin_settings')
+  ],
+  [Markup.button.callback('🔙 Retour', 'back_main')]
 ]);
 
 export const referralKeyboard = Markup.inlineKeyboard([
-  [Markup.button.callback('Share', 'share_referral')],
-  [Markup.button.callback('🔙 Back', 'back_main')]
+  [Markup.button.callback('📤 Partager', 'share_referral')],
+  [Markup.button.callback('📊 Mes parrainages', 'my_referrals')],
+  [Markup.button.callback('🔙 Retour', 'back_main')]
 ]);
